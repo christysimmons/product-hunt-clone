0c14fa4f14d709cbd955ef61784e2768f44a16da
Christina Morrison <ceemo-home-app>
2020-04-30 19:00:37 -0700

    finished product app

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -130,3 +130,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'producthunt/static/'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='icons/')
+    body = models.CharField(max_length=5000)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def pub_date_pretty(self):
+        return self.pub_date.date()
+
+    def summary(self):
+        return self.body[:150]
